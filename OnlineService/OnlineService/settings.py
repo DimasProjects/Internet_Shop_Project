0177739e68a452e57e479ff20b9d226cf209070d
Dmitriy <dima0294@yandex.ru>
2015-10-30 01:14:14 +0300

    Созданы модели для сервиса

--- OnlineService/OnlineService/settings.py
+++ OnlineService/OnlineService/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'OnlineService.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    #    'default': {
-    #        'ENGINE': 'django.db.backends.sqlite3',
-    #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #    }
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'ServiceApp',
+            'USER': 'root',
+            'PASSWORD': 'dimasbmwlover',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
 }
 
 
